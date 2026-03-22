e0f62c8df552265c134a2b4ca4a907d16948bf56
Tar2022 <114656054+Tar2022@users.noreply.github.com>
2026-03-22 15:29:07 +0530

    Create src/App.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pathfinding Visualizer</h1>
+      <p>Welcome to your React app!</p>
+    </div>
+  )
+}
+
+export default App
